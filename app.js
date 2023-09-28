4614ed7ec132566dd6a6d3e1ea9254897d9df9c8
Catalin Pirvulescu <igunereve@gmail.com>
2023-09-28 19:56:45 +0300

    animation

--- app.js
+++ app.js
@@ -11,3 +11,22 @@ window.addEventListener("scroll", (e) => {
     navbar.classList.remove("sticky");
   }
 });
+
+// Gallery
+const images = [
+  "imgs/1.png",
+  "imgs/3.png",
+  "imgs/4.png",
+  "imgs/2.png",
+  "imgs/5.png",
+];
+
+const container = document.getElementById("container");
+
+function renderImg() {
+  for (let i = 0; i < images.length; i++) {
+    container.innerHTML += `<div class="gall-img" style="background-image: url(${images[i]})">`;
+  }
+}
+
+renderImg();
