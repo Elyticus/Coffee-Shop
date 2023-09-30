bb675642c284e0f7e412139fd5522b3d0e81fb5d
Catalin Pirvulescu <igunereve@gmail.com>
2023-09-30 04:02:22 +0300

    cookies

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const navbar = document.querySelector(".navbar");
 const main = document.querySelector("main");
-
 const mainPos = main.getBoundingClientRect().top;
 
 // Navbar
@@ -31,3 +30,49 @@ function renderImg() {
 }
 
 renderImg();
+
+// Cookie Setup
+const cookieModal = document.getElementById("modal");
+const bodyElement = document.getElementById("container-main");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+const consentForm = document.getElementById("login-form");
+const innerText = document.getElementById("modal-inner");
+const declineBtn = document.getElementById("decline-btn");
+const choiceBtn = document.getElementById("modal-choice-btns");
+
+setTimeout(() => {
+  cookieModal.style.display = "inline";
+}, 1000);
+
+bodyElement.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  cookieModal.style.display = "none";
+  bodyElement.classList.remove("blur");
+});
+
+declineBtn.addEventListener("mouseover", () => {
+  choiceBtn.classList.toggle("reverse");
+});
+
+consentForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formLoginData = new FormData(consentForm);
+  const name = formLoginData.get("fullName");
+
+  setTimeout(() => {
+    innerText.innerHTML = `
+    <div class="inner-flex">
+      <h2 class="display-name">
+      Thanks <span class="modal-display-name">${name}</span>!
+      </h2>
+      <p>We just sold the rights to your eternal soul.</p>
+    </div>
+    `;
+
+    modalCloseBtn.disabled = false;
+  }, 0);
+});
