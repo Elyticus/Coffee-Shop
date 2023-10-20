eb5fa6c14b385130403b701c40050707d1adcf9c
Catalin Pirvulescu <igunereve@gmail.com>
2023-10-20 19:41:19 +0300

    news

--- contact.js
+++ contact.js
@@ -9,6 +9,7 @@ const selectDay = document.getElementById("day");
 
 const reserveForm = document.getElementById("form");
 const thankMessage = document.getElementById("thank-message");
+const errorMessage = document.querySelector(".error-message");
 
 reserveForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -18,10 +19,14 @@ reserveForm.addEventListener("submit", (e) => {
     (inputEmail.value = inputEmail.value) &&
     (inputPhone.value = inputPhone.value) &&
     (messageArea.value = messageArea.value) &&
+    messageArea.value.length < 50 &&
     (selectSeats.value = selectSeats.value) &&
     (selectTime.value = selectTime.value) &&
     (selectDay.value = selectDay.value)
   ) {
+    messageArea.value = messageArea.value.slice(0, 50);
+    errorMessage.style.display = "block";
+  } else {
     reserveForm.style.display = "none";
     thankMessage.style.display = "block";
   }
